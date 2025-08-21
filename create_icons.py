de810c297620d1e109feaabf2ea5941be10a906d
Thiemo Ottersbach <thiemo.ott@gmail.com>
2025-08-21 12:52:00 +0200

    chore: switch to black formatting

--- create_icons.py
+++ create_icons.py
@@ -3,15 +3,15 @@ import json
 import os
 import subprocess
 
-ICON_FOLDER = 'src/icons/'
+ICON_FOLDER = "src/icons/"
 
-with open('src/manifest.json', 'r') as manifest:
+with open("src/manifest.json", "r") as manifest:
     manifest_decoded = json.load(manifest)
 
 icon_sizes = manifest_decoded["icons"].keys()
 
-if (os.path.exists(ICON_FOLDER)):
-    old_icons = glob.glob(f'{ICON_FOLDER}icon*.png')
+if os.path.exists(ICON_FOLDER):
+    old_icons = glob.glob(f"{ICON_FOLDER}icon*.png")
     for old_icon in old_icons:
         os.unlink(old_icon)
 else:
@@ -19,5 +19,15 @@ else:
 
 for icon_size in icon_sizes:
     subprocess.run(
-        ['ffmpeg', '-i', 'icon.png', '-vf', f'scale={icon_size}:{icon_size}', f'{ICON_FOLDER}icon{icon_size}.png',
-         '-y'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        [
+            "ffmpeg",
+            "-i",
+            "icon.png",
+            "-vf",
+            f"scale={icon_size}:{icon_size}",
+            f"{ICON_FOLDER}icon{icon_size}.png",
+            "-y",
+        ],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT,
+    )
--- create_zip.py
+++ create_zip.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import shutil
 
-
-shutil.make_archive('soundloud-artwork-viewer-downloader', 'zip', str(Path(__file__).parent)+'/src')
+shutil.make_archive(
+    "soundloud-artwork-viewer-downloader", "zip", str(Path(__file__).parent) + "/src"
+)
