a600e136a41214723019481f68be31674c55cf6f
Thiemo Ottersbach <thiemo.ott@gmail.com>
2025-08-21 12:37:06 +0200

    fix: read icon sizes directly from manifset

--- create_icons.py
+++ create_icons.py
@@ -1,12 +1,16 @@
 import glob
+import json
 import os
 import subprocess
 
 ICON_FOLDER = 'src/icons/'
 
-icon_sizes = [16,32,48,128]
+with open('src/manifest.json', 'r') as manifest:
+    manifest_decoded = json.load(manifest)
 
-if(os.path.exists(ICON_FOLDER)):
+icon_sizes = manifest_decoded["icons"].keys()
+
+if (os.path.exists(ICON_FOLDER)):
     old_icons = glob.glob(f'{ICON_FOLDER}icon*.png')
     for old_icon in old_icons:
         os.unlink(old_icon)
@@ -14,4 +18,6 @@ else:
     os.mkdir(ICON_FOLDER)
 
 for icon_size in icon_sizes:
-    subprocess.run(['ffmpeg', '-i' ,'icon.png','-vf' , f'scale={icon_size}:{icon_size}', f'{ICON_FOLDER}icon{icon_size}.png', '-y'])
+    subprocess.run(
+        ['ffmpeg', '-i', 'icon.png', '-vf', f'scale={icon_size}:{icon_size}', f'{ICON_FOLDER}icon{icon_size}.png',
+         '-y'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
